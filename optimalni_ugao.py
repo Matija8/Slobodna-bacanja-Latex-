cb19ca5cf09fec4919c6572c12caa40ee090ca87
Matija8 <matijanme@gmail.com>
2019-05-30 15:47:13 +0200

    Popravljen pajton kod

--- optimalni_ugao.py
+++ optimalni_ugao.py
@@ -4,33 +4,39 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-#TODO sredi module (izbaci main, sredi brojke)
 
 def optimalni_ugao(d_c, r_l, r_o, g, H):
-    min_teta = max(atan(H/d_c) + prebaci_u_radijan(5), prebaci_u_radijan(45))
-    max_teta = prebaci_u_radijan(60)
+    min_teta = atan(H/d_c) + prebaci_u_radijan(1)
+    max_teta = prebaci_u_radijan(90)
     opt_teta = min_teta
     opt_razlika = 0
     start = time.time()
+
     for ugao in np.arange(min_teta, max_teta, 0.0001):
         teta1, teta2 = nadji_teta1_teta2(ugao, d_c, r_l, r_o, g, H)
+        # ako sklonimo komentar ispod mozemo da vidimo teta2-teta1 za svaki ugao
+        print(f"teta = {prebaci_u_stepen(ugao):.2f}, raz = {prebaci_u_stepen(teta2 - teta1):.6f}")
         if teta2 - teta1 > opt_razlika:
             opt_teta = ugao
             opt_razlika = teta2 - teta1
+
     end = time.time()
-    #print(end - start)
+    print(f"Vreme trajanja programa: {(end - start):.4} s")  # ispisuje vreme trajanja programa u sekundama
     return opt_teta
 
 
-
 if __name__ == "__main__":
-    d_c = 4.15      # daljina do centra obruca
-    r_l = 0.1213    # poluprecnik lopte
-    r_o = 0.23      # poluprecnik obruca
-    g = 9.81        # gravitaciona konstanta
-    hk = 3.05       # visina kosa
-    h = 1.95        # visina kosarkasa TODO
-    H = hk-5.0/4*h  # popravljena visina kosa
+    d_c = 4.57-0.15-0.23  # daljina do centra obruca
+    r_l = 0.75/(2*pi)     # poluprecnik lopte
+    r_o = 0.23            # poluprecnik obruca
+    g = 9.81              # gravitaciona konstanta
+    hk = 3.05             # visina kosa
+
+    h = float(input("Unesite visinu kosarksa (u metrima): "))
+    if not 0 < h < 3:
+        print("Visina kosarkasa nije u korektnom rasponu (do 3 metra)")
+        exit()
+    H = hk-5.0/4*h        # popravljena visina kosa
 
     teta = optimalni_ugao(d_c, r_l, r_o, g, H)
-    print(prebaci_u_stepen(teta))
+    print(f"\nOptimalni ugao teta = {prebaci_u_stepen(teta):.4f}")
--- teta1_2.py
+++ teta1_2.py
@@ -5,26 +5,26 @@ import matplotlib.pyplot as plt
 
 def nadji_teta1_teta2(teta, d_c, r_l, r_o, g, H):
     # uslov (8)
-    if not atan(H/d_c) < teta < pi/2:
+    if not atan(H/d_c) < teta < prebaci_u_radijan(90):
         return teta, teta
-    v = nadji_brzinu(d_c, teta, H, g)
 
-    min_teta = max(asin(sqrt(2*g*H/v**2)), atan(H/d_c))
-    max_teta = prebaci_u_radijan(85)    # TODO
+    v = nadji_brzinu(d_c, teta, H, g)
+    min_teta = max(asin(sqrt(2*g*H/v**2)), atan(H/d_c)) + prebaci_u_radijan(0.1)
+    max_teta = prebaci_u_radijan(90)
     ugao_korak = 0.001
     t1 = (d_c - r_o - r_l) / (v * cos(teta))
     t2 = (d_c - r_o + r_l) / (v * cos(teta))
     t_korak = 0.1
     teta1 = teta2 = teta
     stani = False
-    #print( nadji_distancu(v, teta, H, g)) TODO
 
     for ugao in np.arange(teta, min_teta, -ugao_korak):
-        if not d_c - r_o + r_l < nadji_distancu(v, ugao, H, g) < d_c + r_o - r_l:
+        if not d_c - r_o + r_l < nadji_distancu(v, ugao-ugao_korak, H, g) < d_c + r_o - r_l:
             teta1 = ugao
             break
         for t in np.arange(t1, t2, t_korak):  # TODO
-            if not ((x_koord(t, v, ugao) - (d_c - r_o))**2 + (y_koord(t, v, ugao, g))**2 > r_l**2):
+            if not (((x_koord(t, v, ugao-ugao_korak) - (d_c - r_o))**2
+                    + (y_koord(t, v, ugao-ugao_korak, g) - H)**2) > (r_l**2)):
                 teta1 = ugao
                 stani = True
                 break
@@ -34,11 +34,12 @@ def nadji_teta1_teta2(teta, d_c, r_l, r_o, g, H):
     stani = False
 
     for ugao in np.arange(teta, max_teta, ugao_korak):
-        if not d_c - r_o + r_l < nadji_distancu(v, ugao, H, g) < d_c + r_o - r_l:
+        if not d_c - r_o + r_l < nadji_distancu(v, ugao+ugao_korak, H, g) < d_c + r_o - r_l:
             teta2 = ugao
             break
         for t in np.arange(t1, t2, t_korak):  # TODO
-            if not ((x_koord(t, v, ugao) - (d_c - r_o)) ** 2 + (y_koord(t, v, ugao, g)) ** 2 > r_l ** 2):
+            if not (((x_koord(t, v, ugao+ugao_korak) - (d_c - r_o)) ** 2
+                    + (y_koord(t, v, ugao+ugao_korak, g) - H) ** 2) > (r_l ** 2)):
                 teta2 = ugao
                 stani = True
                 break
@@ -69,19 +70,25 @@ def nadji_brzinu(d, teta, y, g):
 
 
 def nadji_distancu(v, teta, H, g):
-    #proveru (5) vrsimo pre poziva
     return v*cos(teta)/g*(v*sin(teta)+sqrt(v**2*sin(teta)**2 - 2*g*H))
 
 
 if __name__ == "__main__":
-    d_c = 4.15      # daljina do centra obruca
-    r_l = 0.1213    # poluprecnik lopte
-    r_o = 0.23      # poluprecnik obruca
-    g = 9.81        # gravitaciona konstanta
-    hk = 3.05       # visina kosa
-    h = 1.95        # visina kosarkasa
-    H = hk-5.0/4*h  # popravljena visina kosa
+    d_c = 4.57-0.15-0.23  # daljina do centra obruca
+    r_l = 0.75/(2*pi)     # poluprecnik lopte
+    r_o = 0.23            # poluprecnik obruca
+    g = 9.81              # gravitaciona konstanta
+    hk = 3.05             # visina kosa
 
-    teta = prebaci_u_radijan(30)
-    (teta_1, teta_2) = nadji_teta1_teta2(teta, d_c, r_l, r_o, g, H)
-    print(prebaci_u_stepen(teta_1), prebaci_u_stepen(teta_2))
+    h = float(input("Unesite visinu kosarksa (u metrima): "))
+    if not 0 < h < 3:
+        print("Visina kosarkasa nije u korektnom rasponu (do 3 metra)!")
+        exit()
+    H = hk-5.0/4*h        # popravljena visina kosa
+
+    teta = float(input("Unesite ugao teta (u stepenima): "))
+    if not 0 < teta < 90:
+        print("Ugao mora biti izmedju 0 i 90!")
+        exit()
+    (teta_1, teta_2) = nadji_teta1_teta2(prebaci_u_radijan(teta), d_c, r_l, r_o, g, H)
+    print(f"teta1 = {prebaci_u_stepen(teta_1):.2f}, teta2 = {prebaci_u_stepen(teta_2):.2f}")
