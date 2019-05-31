d89ba1b774dfec72a32723d6bc968dfb2cb9f467
Matija8 <matijanme@gmail.com>
2019-05-31 22:04:53 +0200

    Sredjene primedbe profesora. Kod ima problema, treba srediti kod i opet izracunati tabelu

--- optimalni_ugao.py
+++ optimalni_ugao.py
@@ -6,27 +6,28 @@ import time
 
 
 def optimalni_ugao(d_c, r_l, r_o, g, H):
-    min_teta = atan(H/d_c) + prebaci_u_radijan(1)
-    max_teta = prebaci_u_radijan(90)
+    min_teta = max(atan(H/d_c) + prebaci_u_radijan(1), prebaci_u_radijan(20))
+    max_teta = prebaci_u_radijan(70)  # moze i do 90 ali nema potrebe
     opt_teta = min_teta
     opt_razlika = 0
+    korak_teta = prebaci_u_radijan(0.01)  # 0.01 stepen nam je korak
     start = time.time()
 
-    for ugao in np.arange(min_teta, max_teta, 0.0001):
+    for ugao in np.arange(min_teta, max_teta, korak_teta):
         teta1, teta2 = nadji_teta1_teta2(ugao, d_c, r_l, r_o, g, H)
         # ako sklonimo komentar ispod mozemo da vidimo teta2-teta1 za svaki ugao
-        #print(f"teta = {prebaci_u_stepen(ugao):.2f}, raz = {prebaci_u_stepen(teta2 - teta1):.6f}")
+        print(f"teta = {prebaci_u_stepen(ugao):.2f}, raz = {prebaci_u_stepen(teta2 - teta1):.3f}")
         if teta2 - teta1 > opt_razlika:
             opt_teta = ugao
             opt_razlika = teta2 - teta1
 
     end = time.time()
-    #print(f"Vreme trajanja programa: {(end - start):.4} s")  # ispisuje vreme trajanja programa u sekundama
+    print(f"Vreme trajanja programa: {(end - start):.4} s")  # ispisuje vreme trajanja programa u sekundama
     return opt_teta
 
 
 if __name__ == "__main__":
-    d_c = 4.57-0.15-0.23  # daljina do centra obruca
+    d_c = 4.19            # daljina do centra obruca
     r_l = 0.75/(2*pi)     # poluprecnik lopte
     r_o = 0.23            # poluprecnik obruca
     g = 9.81              # gravitaciona konstanta
--- teta1_2.py
+++ teta1_2.py
@@ -5,48 +5,52 @@ import matplotlib.pyplot as plt
 
 def nadji_teta1_teta2(teta, d_c, r_l, r_o, g, H):
     # uslov (8)
-    if not atan(H/d_c) < teta < prebaci_u_radijan(90):
+    if not atan(H/d_c) + prebaci_u_radijan(1) < teta < prebaci_u_radijan(90):
         return teta, teta
 
     v = nadji_brzinu(d_c, teta, H, g)
-    min_teta = max(asin(sqrt(2*g*H/v**2)), atan(H/d_c)) + prebaci_u_radijan(0.1)
+    min_teta = max(asin(sqrt(2*g*H/v**2)), atan(H/d_c)) + prebaci_u_radijan(1)
     max_teta = prebaci_u_radijan(90)
-    ugao_korak = 0.001
-    t1 = (d_c - r_o - r_l) / (v * cos(teta))
-    t2 = (d_c - r_o + r_l) / (v * cos(teta))
-    t_korak = 0.1
-    teta1 = teta2 = teta
+
+    ugao_korak = prebaci_u_radijan(1)  # prvo nam je 1 stepen korak
+    teta1 = uslovi_12_i_11(teta, min_teta, -ugao_korak, d_c, r_l, r_o, g, H, v)
+
+    ugao_korak = prebaci_u_radijan(0.01)  # zatim 0.01 stepen, ali sad idemo od teta1
+    teta1 = uslovi_12_i_11(teta1, min_teta, -ugao_korak, d_c, r_l, r_o, g, H, v)
+
+    ugao_korak = prebaci_u_radijan(1)  # prvo nam je 1 stepen korak
+    teta2 = uslovi_12_i_11(teta, max_teta, ugao_korak, d_c, r_l, r_o, g, H, v)
+
+    ugao_korak = prebaci_u_radijan(0.01)  # zatim trazimo preciznije od teta2 do max_teta
+    teta2 = uslovi_12_i_11(teta2, max_teta, ugao_korak, d_c, r_l, r_o, g, H, v)
+
+    return teta1, teta2
+
+
+def uslovi_12_i_11(od, do, korak, d_c, r_l, r_o, g, H, v):
+    teta1_2 = od
+
     stani = False
 
-    for ugao in np.arange(teta, min_teta, -ugao_korak):
-        if not d_c - r_o + r_l < nadji_distancu(v, ugao-ugao_korak, H, g) < d_c + r_o - r_l:
-            teta1 = ugao
+    # uslov (11)
+    for ugao in np.arange(od, do, korak):
+        if not d_c - r_o + r_l < nadji_distancu(v, ugao+korak, H, g) < d_c + r_o - r_l: #TODO
+            teta1_2 = ugao
             break
+
+        # uslov (12)
+        t1 = (d_c - r_o - r_l) / (v * cos(ugao+korak))
+        t2 = (d_c - r_o + r_l) / (v * cos(ugao+korak))
+        t_korak = (t2-t1) / 10  # imacemo 9 provera da li t kaci obruc
         for t in np.arange(t1, t2, t_korak):
-            if not (((x_koord(t, v, ugao-ugao_korak) - (d_c - r_o))**2
-                    + (y_koord(t, v, ugao-ugao_korak, g) - H)**2) > (r_l**2)):
-                teta1 = ugao
+            if not (((x_koord(t, v, ugao+korak) - (d_c - r_o))**2
+                    + (y_koord(t, v, ugao+korak, g) - H)**2) > (r_l**2)):
+                teta1_2 = ugao
                 stani = True
                 break
         if stani is True:
             break
-
-    stani = False
-
-    for ugao in np.arange(teta, max_teta, ugao_korak):
-        if not d_c - r_o + r_l < nadji_distancu(v, ugao+ugao_korak, H, g) < d_c + r_o - r_l:
-            teta2 = ugao
-            break
-        for t in np.arange(t1, t2, t_korak):
-            if not (((x_koord(t, v, ugao+ugao_korak) - (d_c - r_o)) ** 2
-                    + (y_koord(t, v, ugao+ugao_korak, g) - H) ** 2) > (r_l ** 2)):
-                teta2 = ugao
-                stani = True
-                break
-            if stani is True:
-                break
-
-    return teta1, teta2
+    return teta1_2
 
 
 def prebaci_u_stepen(alfa):
@@ -74,7 +78,7 @@ def nadji_distancu(v, teta, H, g):
 
 
 if __name__ == "__main__":
-    d_c = 4.57-0.15-0.23  # daljina do centra obruca
+    d_c = 4.19            # daljina do centra obruca
     r_l = 0.75/(2*pi)     # poluprecnik lopte
     r_o = 0.23            # poluprecnik obruca
     g = 9.81              # gravitaciona konstanta
